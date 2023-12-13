893d11920b9086d7ce2185c94451d5d4bc67f434
Ibrahim_ingushNeft <itochiev@ing-inform.local>
2023-12-13 12:44:29 +0300

    test

--- script.js
+++ script.js
@@ -1,118 +1,125 @@
-const addBtn = document.querySelector('.btn-add');
-const body = document.querySelector('body');
-const main = document.querySelector('.main');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const pages = document.getElementById('pages');
-const progress = document.getElementById('progress');
-
-
+const addBtn = document.querySelector(".btn-add");
+const body = document.querySelector("body");
+const main = document.querySelector(".main");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const progress = document.getElementById("progress");
 
 const myLibrary = [];
 
-function Book (title, author, pages, readindgStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readindgStatus = readindgStatus;
-    this.info = function () {
-        return `${this.title} by ${ this.author}, ${this.pages} pages, ${this.readindgStatus}.`;
-    }
+function Book(title, author, pages, readindgStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readindgStatus = readindgStatus;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readindgStatus}.`;
+  };
 }
 
-
-
 //add several books to lybrary for display
-const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 295, 'not read yet');
-const harryPotter = new Book('Harry Potter', 'J. K. Rowling', 305, 'read');
-const toKillMockingbird = new Book('To Kill a Mockingbird', 'H. Lee', 281, 'read');
-const theIdiot = new Book('The Idiot', 'F. Dostoevsky', 768, 'not read yet');
+const theHobbit = new Book(
+  "The Hobbit",
+  "J. R. R. Tolkien",
+  295,
+  "not read yet"
+);
+const harryPotter = new Book("Harry Potter", "J. K. Rowling", 305, "read");
+const toKillMockingbird = new Book(
+  "To Kill a Mockingbird",
+  "H. Lee",
+  281,
+  "read"
+);
+const theIdiot = new Book("The Idiot", "F. Dostoevsky", 768, "not read yet");
 
-myLibrary.push(theHobbit,harryPotter,toKillMockingbird,theIdiot);
+myLibrary.push(theHobbit, harryPotter, toKillMockingbird, theIdiot);
 
 //display books
 
-function displayBook (arr) {
-    for(let i = 0; i< arr.length; i++) {
-        if (!(arr[i])) {
-            continue;
-        }
-        const card = document.createElement("div");
-        card.innerText = arr[i].info();
-        card.dataset.bigDick = `${i+1}`;
-        main.appendChild(card);
-        const btn = document.createElement('button');
-        btn.innerText = 'Remove';
-        btn.dataset.bigDick = `${i+1}`;
-        card.appendChild(btn);
-        
+function displayBook(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i]) {
+      continue;
     }
-};
+    const card = document.createElement("div");
+    card.innerText = arr[i].info();
+    card.dataset.bigDick = `${i + 1}`;
+    main.appendChild(card);
+    const btn = document.createElement("button");
+    btn.innerText = "Remove";
+    btn.dataset.bigDick = `${i + 1}`;
+    card.appendChild(btn);
+  }
+}
 
 displayBook(myLibrary);
 
 //remove button
 
-const removeBtn = document.querySelectorAll('button[data-big-dick]');
-removeBtn.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-       console.log(e.target.dataset.bigDick);
-       delete myLibrary[(e.target.dataset.bigDick) - 1];
-       console.log(myLibrary);
-       console.log(e.target);
-       console.log(e.target.closest("div[data-big-dick]"));
-       main.removeChild(e.target.closest("div[data-big-dick]"))
+const removeBtn = document.querySelectorAll("button[data-big-dick]");
+removeBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e.target.dataset.bigDick);
+    delete myLibrary[e.target.dataset.bigDick - 1];
+    console.log(myLibrary);
+    console.log(e.target);
+    console.log(e.target.closest("div[data-big-dick]"));
+    main.removeChild(e.target.closest("div[data-big-dick]"));
     //    displayBook(myLibrary);
-        console.log(myLibrary);
-    })
-})
+    console.log(myLibrary);
+  });
+});
 console.log(myLibrary);
 
 //add book to the array
 
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const newBook = new Book(title.value, author.value, pages.value, progress.value)
-    myLibrary.push(newBook);
-    // displayBook(myLibrary);
-    addBook(myLibrary);
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    progress.value
+  );
+  myLibrary.push(newBook);
+  // displayBook(myLibrary);
+  addBook(myLibrary);
 
-    document.querySelector('form').reset();
+  document.querySelector("form").reset();
 
-    console.log(myLibrary[myLibrary.length - 1]);
-})
-
-
-addBtn.addEventListener('click', function () {
-    
+  console.log(myLibrary[myLibrary.length - 1]);
 });
+
+addBtn.addEventListener("click", function () {});
 
 // create card
 
-function addBook (arr) {
-    const card = document.createElement("div");
-    card.innerText = arr[arr.length - 1].info();
-    card.dataset.bigDick = `${arr.length}`;
-    main.appendChild(card);
-    const btn = document.createElement('button');
-    btn.innerText = 'Remove';
-    btn.dataset.bigDick = `${arr.length}`;
-    card.appendChild(btn);
+function addBook(arr) {
+  const card = document.createElement("div");
+  card.innerText = arr[arr.length - 1].info();
+  card.dataset.bigDick = `${arr.length}`;
+  main.appendChild(card);
+  const btn = document.createElement("button");
+  btn.innerText = "Remove";
+  btn.dataset.bigDick = `${arr.length}`;
+  card.appendChild(btn);
 
-    btn.addEventListener('click', (e) => {
-        delete myLibrary[(e.target.dataset.bigDick) - 1];
-        main.removeChild(e.target.closest("div[data-big-dick]"))
-     })
+  btn.addEventListener("click", (e) => {
+    delete myLibrary[e.target.dataset.bigDick - 1];
+    main.removeChild(e.target.closest("div[data-big-dick]"));
+  });
 
-     console.log(myLibrary);
+  console.log(myLibrary);
 }
-
 
 //remove card button
 
-function removeCard (btn, e) {
-    btn.addEventListener('click', (e) => {
-        delete myLibrary[(e.target.dataset.bigDick) - 1];
-        main.removeChild(e.target.closest("div[data-big-dick]"))
-     })
+function removeCard(btn, e) {
+  btn.addEventListener("click", (e) => {
+    delete myLibrary[e.target.dataset.bigDick - 1];
+    main.removeChild(e.target.closest("div[data-big-dick]"));
+  });
 }
+
